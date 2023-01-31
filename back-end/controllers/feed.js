6003a92ad435e356c70b8ba4867d38a1f1c445a4
GiannisTzonis <tzonis.john@hotmail.com>
2023-01-31 06:52:10 +0200

    added post model and server side validation

--- back-end/controllers/feed.js
+++ back-end/controllers/feed.js
@@ -21,6 +21,12 @@ exports.createPost = (req, res, next) => {
   // Create post in db
   res.status(201).json({
     message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: 'Maximilian' },
+      createdAt: new Date()
+    }
   });
 };
